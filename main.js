c4948f2bc10082d88d56f4e34791bf4f5eb29f9c
bhavanishpallamala <bhp4002@med.cornell.edu>
2023-09-15 05:18:27 -0400

    a

--- main.js
+++ main.js
@@ -1,37 +1,32 @@
-// This is free and unencumbered software released into the public domain.
-// See LICENSE for details
-
-const {app, BrowserWindow, Menu} = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
 const log = require('electron-log');
-const config = require('config')
-const { autoUpdater } = require("electron-updater");
-const AWS = require("aws-sdk")
-const fs = require("fs");
-const aws4 = require("aws4");
-const bucketname = 'bhavanish1'
+const { autoUpdater } = require('electron-updater');
+const AWS = require('aws-sdk');
+const aws4 = require('aws4');
+
+const bucketname = 'bhavanish1';
 
 AWS.config.update({
-  accessKeyId : process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey : process.env.AWS_SECRET_ACCESS_KEY,
-  region: "ap-south-1"
-})
-log.info("AWS.config.update.accessKeyId: ", process.env.AWS_ACCESS_KEY_ID);
-log.info("AWS.config.update.secretAccessKeyId: ",process.env.AWS_SECRET_ACCESS_KEY);
-
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: 'ap-south-1',
+});
+log.info('AWS.config.update.accessKeyId: ', process.env.AWS_ACCESS_KEY_ID);
+log.info('AWS.config.update.secretAccessKeyId: ', process.env.AWS_SECRET_ACCESS_KEY);
 
 const s3 = new AWS.S3();
 
 function listS3Objects() {
   const params = {
     Bucket: bucketname,
-    Prefix: "etime/desktop",
+    Prefix: 'etime/desktop',
   };
 
   s3.listObjects(params, (err, data) => {
     if (err) {
-      log.error("Error listing objects:", err);
+      log.error('Error listing objects:', err);
     } else {
-      console.log("Objects in the S3 bucket:");
+      console.log('Objects in the S3 bucket:');
       for (const obj of data.Contents) {
         log.info(obj.Key);
       }
@@ -40,75 +35,45 @@ function listS3Objects() {
 }
 listS3Objects();
 
-autoUpdater.setFeedURL({
-  provider: 's3',
-  url: 's3://bhavanish1/etime/desktop',
-  channel: 'latest', 
-  config: {
-    region: 'ap-south-1',
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  },
-});
-//-------------------------------------------------------------------
-// Logging
-//
-// THIS SECTION IS NOT REQUIRED
-//
-// This logging setup is not required for auto-updates to work,
-// but it sure makes debugging easier :)
-//-------------------------------------------------------------------
+
 autoUpdater.logger = log;
 autoUpdater.logger.transports.file.level = 'info';
 log.info('App starting...');
 
-//-------------------------------------------------------------------
-// Define the menu
-//
-// THIS SECTION IS NOT REQUIRED
-//-------------------------------------------------------------------
-let template = []
+let template = [];
 if (process.platform === 'darwin') {
-  // OS X
   const name = app.getName();
   template.unshift({
     label: name,
     submenu: [
       {
         label: 'About ' + name,
-        role: 'about'
+        role: 'about',
       },
       {
         label: 'Quit',
         accelerator: 'Command+Q',
-        click() { app.quit(); }
+        click() {
+          app.quit();
+        },
       },
-    ]
-  })
+    ],
+  });
 }
 
-
-//-------------------------------------------------------------------
-// Open a window that displays the version
-//
-// THIS SECTION IS NOT REQUIRED
-//
-// This isn't required for auto-updates to work, but it's easier
-// for the app to show a window than to have to click "About" to see
-// that updates are working.
-//-------------------------------------------------------------------
 let win;
 
 function sendStatusToWindow(text) {
   log.info(text);
   win.webContents.send('message', text);
 }
+
 function createDefaultWindow() {
   win = new BrowserWindow({
     webPreferences: {
       nodeIntegration: true,
-      contextIsolation: false
-    }
+      contextIsolation: false,
+    },
   });
   win.webContents.openDevTools();
   win.on('closed', () => {
@@ -117,90 +82,85 @@ function createDefaultWindow() {
   win.loadURL(`file://${__dirname}/version.html#v${app.getVersion()}`);
   return win;
 }
-autoUpdater.on("checking-for-update", async () => {
+
+autoUpdater.on('checking-for-update', async () => {
   let opts = {
-    region: "ap-south-1",
-    protocol: "https:",
-    method: "get",
-    hostname: "bhavanish1.s3.amazonaws.com",
-    path: "/etime/desktop/latest.yml",
-    host: "s3-ap-south-1.amazonaws.com"
+    region: 'ap-south-1',
+    protocol: 'https:',
+    method: 'get',
+    hostname: 'bhavanish1.s3.amazonaws.com',
+    path: '/etime/desktop/latest.yml',
+    host: 's3-ap-south-1.amazonaws.com',
   };
 
   await aws4.sign(opts, {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   });
 
   autoUpdater.requestHeaders = opts.headers;
 });
+
 autoUpdater.on('update-available', (info) => {
+  // Handle update availability here
   sendStatusToWindow('Update available.');
-})
+
+  // Define your release path variable elsewhere
+  let update_path = '/etime/desktop/latest.yml';
+  let opts = {
+    service: 's3',
+    region: 'ap-south-1', // Use the correct region for your S3 bucket
+    method: 'GET',
+    host: 'bhavanish1.s3.amazonaws.com', // Use your bucket name
+    path: update_path,
+  };
+  aws4.sign(opts, {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  });
+  autoUpdater.requestHeaders = opts.headers;
+
+  // Start downloading the update
+  autoUpdater.downloadUpdate();
+});
+
 autoUpdater.on('update-not-available', (info) => {
   sendStatusToWindow('Update not available.');
-})
+});
+
 autoUpdater.on('error', (err) => {
   sendStatusToWindow('Error in auto-updater. ' + err);
-})
+});
+
 autoUpdater.on('download-progress', (progressObj) => {
-  let log_message = "Download speed: " + progressObj.bytesPerSecond;
+  let log_message = 'Download speed: ' + progressObj.bytesPerSecond;
   log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
-  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+  log_message = log_message + ' (' + progressObj.transferred + '/' + progressObj.total + ')';
   sendStatusToWindow(log_message);
-})
+});
+
 autoUpdater.on('update-downloaded', (info) => {
   sendStatusToWindow('Update downloaded');
 });
-app.on('ready', function() {
-  // Create the Menu
+
+app.on('ready', function () {
   const menu = Menu.buildFromTemplate(template);
   Menu.setApplicationMenu(menu);
 
   createDefaultWindow();
 });
+
 app.on('window-all-closed', () => {
   app.quit();
 });
 
-//
-// CHOOSE one of the following options for Auto updates
-//
-
-//-------------------------------------------------------------------
-// Auto updates - Option 1 - Simplest version
-//
-// This will immediately download an update, then install when the
-// app quits.
-//-------------------------------------------------------------------
-app.on('ready', function()  {
-  autoUpdater.checkForUpdatesAndNotify();
+app.on('activate', () => {
+  if (win === null) {
+    createDefaultWindow();
+  }
 });
 
-//-------------------------------------------------------------------
-// Auto updates - Option 2 - More control
-//
-// For details about these events, see the Wiki:
-// https://github.com/electron-userland/electron-builder/wiki/Auto-Update#events
-//
-// The app doesn't need to listen to any events except `update-downloaded`
-//
-// Uncomment any of the below events to listen for them.  Also,
-// look in the previous section to see them being used.
-//-------------------------------------------------------------------
-// app.on('ready', function()  {
-//   autoUpdater.checkForUpdates();
-// });
-// autoUpdater.on('checking-for-update', () => {
-// })
-// autoUpdater.on('update-available', (info) => {
-// })
-// autoUpdater.on('update-not-available', (info) => {
-// })
-// autoUpdater.on('error', (err) => {
-// })
-// autoUpdater.on('download-progress', (progressObj) => {
-// })
-// autoUpdater.on('update-downloaded', (info) => {
-//   autoUpdater.quitAndInstall();  
-// })
+// Auto updates - Option 1 - Simplest version
+app.on('ready', function () {
+  autoUpdater.checkForUpdatesAndNotify();
+});
